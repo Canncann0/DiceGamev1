41fb9887c03054ec7f16e97d8d7b8d30e5d13a2d
Canncann0 <turgutdogancan0@gmail.com>
2024-01-10 22:42:49 +0300

    Initial Commit

---
+++ DiceGamev1/DiceGamev1App.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGamev1App.swift
+//  DiceGamev1
+//
+//  Created by Doğancan Turgut on 10.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceGamev1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
