a317931ec6bd0f8800c93a68d8206b6b312c65ac
Canncann0 <turgutdogancan0@gmail.com>
2024-01-11 23:13:34 +0300

    finishCommit

---
+++ DiceGamev1/Screen/WinScreen.swift
@@ -0,0 +1,18 @@
+//
+//  WinScreen.swift
+//  DiceGamev1
+//
+//  Created by Doğancan Turgut on 11.01.2024.
+//
+
+import SwiftUI
+
+struct WinScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WinScreen()
+}
